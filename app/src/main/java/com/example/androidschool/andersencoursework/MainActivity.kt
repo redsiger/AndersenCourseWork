3367e850b09cd7868580e1601244124dbdd5f610
redsiger <redsiger@yandex.ru>
2022-01-14 18:33:08 +0300

    Added feature_episodes module and multi module navigation preparations

--- app/src/main/java/com/example/androidschool/andersencoursework/MainActivity.kt
+++ app/src/main/java/com/example/androidschool/andersencoursework/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.androidschool.andersencoursework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
---
+++ feature_characters/src/main/java/com/example/androidschool/feature_characters/presenter/CharactersListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.androidschool.feature_characters.presenter
+
+import androidx.fragment.app.Fragment
+import com.example.androidschool.feature_characters.R
+
+class CharactersListFragment: Fragment(R.layout.fragment_characters_list) {
+}
